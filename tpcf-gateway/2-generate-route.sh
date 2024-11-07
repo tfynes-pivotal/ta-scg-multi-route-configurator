f737a761c4997180b1dba5eefad42f776ca2cc1b
Mark Fynes <mark.fynes@broadcom.com>
2024-11-07 14:58:50 -0500

    .

--- tpcf-gateway/2-generate-route.sh
+++ tpcf-gateway/2-generate-route.sh
@@ -11,3 +11,9 @@
 #ADDITIONAL-ROUTES# ytt --data-values-file=route-additions-data.yaml --file-mark route-additions-overlay-template.yaml:type=text-template -f route-additions-overlay-template.yaml --output-files output
 #ADDITIONAL-ROUTES# cp output/route-additions-overlay-template.yaml ./route-additions-overlay.yaml
 #ADDITIONAL-ROUTES# ytt --file-mark 'route-no-added-routes.json:type=yaml-template' -f route-no-added-routes.json -f ./route-additions-overlay.yaml --output json | jq . > ./route.json
+
+#NOTES-REMOVE-ROUTES# cat routes.json | jq 'del(.routes[] | select ( .method == "GET" and .path == "/api/foo"))'
+#NOTES-REMOVE-ROUTES# for i in $(cat route-removals-data.json| jq -c '.routes[]'); do echo $i | jq .method && echo; done
+#NOTES-REMOVE-ROUTES# cat route.json| jq '.routes[] | .method + " " + .path'
+
+#REMOVE-ROUTES# ./delete-filtered-routes.sh
---
+++ tpcf-gateway/delete-filtered-routes.sh
@@ -0,0 +1,10 @@
+cp route.json route-pre-delete.json
+for i in $(cat route-removals-data.json| jq -c '.routes[]'); 
+do
+    m=$(echo $i | jq -r .method)
+    p=$(echo $i | jq -r .path)
+    echo filtering method = $m and path = $p
+    cat route-pre-delete.json | jq "del(.routes[] | select (.method==\"$m\" and .path==\"$p\"))" > route-pre-delete.tmp
+    mv route-pre-delete.tmp route-pre-delete.json
+done
+mv route-pre-delete.json route.json
