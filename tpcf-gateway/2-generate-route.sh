e7c96c8d95d179c95d7de95e32e4eebc9847fdb1
Mark Fynes <mark.fynes@broadcom.com>
2024-11-13 11:39:54 -0500

    okta integration working

--- tpcf-gateway/2-generate-route.sh
+++ tpcf-gateway/2-generate-route.sh
@@ -1,19 +1,19 @@
 #CONVERT-SERVICE-DOC# ytt -f routeConfig.yaml --output json | jq . > routeConfig.json
-#CONVERT-SERVICE-DOC# curl --request POST 'OPENAPI_TO_ROUTE_CONVERSTION_SERVICE_ENDPOINT' --header 'Content-Type: application/json' -d @routeConfig.json | jq . > ./route.json
-#NO-CONVERT-SERVICE-DOC# echo '{"routes": []}' > route.json
+#CONVERT-SERVICE-DOC# curl --request POST 'OPENAPI_TO_ROUTE_CONVERSTION_SERVICE_ENDPOINT' --header 'Content-Type: application/json' -d @routeConfig.json | jq . > ./routes.json
+#NO-CONVERT-SERVICE-DOC# echo '{"routes": []}' > routes.json
 
 
-#CORS-FIX# cp route.json route-no-cors-fixes.json
-#CORS-FIX# ytt --file-mark 'route-no-cors-fixes.json:type=yaml-template' -f ./route-no-cors-fixes.json -f ./route-cors-config-overlay.yaml --output json | jq . > route.json
+#CORS-FIX# cp routes.json route-no-cors-fixes.json
+#CORS-FIX# ytt --file-mark 'route-no-cors-fixes.json:type=yaml-template' -f ./route-no-cors-fixes.json -f ./route-cors-config-overlay.yaml --output json | jq . > routes.json
 
 
-#ADDITIONAL-ROUTES# cp route.json route-no-added-routes.json
+#ADDITIONAL-ROUTES# cp routes.json route-no-added-routes.json
 #ADDITIONAL-ROUTES# ytt --data-values-file=route-additions-data.yaml --file-mark route-additions-overlay-template.yaml:type=text-template -f route-additions-overlay-template.yaml --output-files output
 #ADDITIONAL-ROUTES# cp output/route-additions-overlay-template.yaml ./route-additions-overlay.yaml
-#ADDITIONAL-ROUTES# ytt --file-mark 'route-no-added-routes.json:type=yaml-template' -f route-no-added-routes.json -f ./route-additions-overlay.yaml --output json | jq . > ./route.json
+#ADDITIONAL-ROUTES# ytt --file-mark 'route-no-added-routes.json:type=yaml-template' -f route-no-added-routes.json -f ./route-additions-overlay.yaml --output json | jq . > ./routes.json
 
 #NOTES-REMOVE-ROUTES# cat routes.json | jq 'del(.routes[] | select ( .method == "GET" and .path == "/api/foo"))'
 #NOTES-REMOVE-ROUTES# for i in $(cat route-removals-data.json| jq -c '.routes[]'); do echo $i | jq .method && echo; done
-#NOTES-REMOVE-ROUTES# cat route.json| jq '.routes[] | .method + " " + .path'
+#NOTES-REMOVE-ROUTES# cat routes.json| jq '.routes[] | .method + " " + .path'
 
 #REMOVE-ROUTES# ./delete-filtered-routes.sh
--- tpcf-gateway/delete-filtered-routes.sh
+++ tpcf-gateway/delete-filtered-routes.sh
@@ -1,11 +1,14 @@
-cp route.json route-pre-delete.json
+cp routes.json route-pre-delete.json
+routes=$(<routes.json)
 ytt -f route-removals-data.yaml --output json >  route-removals-data.json
 for i in $(cat route-removals-data.json| jq -c '.routes[]'); 
 do
     m=$(echo $i | jq -r .method)
     p=$(echo $i | jq -r .path)
     echo filtering method = $m and path = $p
-    cat route-pre-delete.json | jq "del(.routes[] | select (.method==\"$m\" and .path==\"$p\"))" > route-pre-delete.tmp
-    mv route-pre-delete.tmp route-pre-delete.json
+    routes=$(echo $routes | jq "del(.routes[] | select (.method==\"$m\" and .path==\"$p\"))") 
+    #cat route-pre-delete.json | jq "del(.routes[] | select (.method==\"$m\" and .path==\"$p\"))" > route-pre-delete.tmp
+    #mv route-pre-delete.tmp route-pre-delete.json
 done
-mv route-pre-delete.json route.json
+#mv route-pre-delete.json routes.json
+echo $routes | jq . > routes.json
