70d2e0ffebb6a1e2281b3bf2d84227793536eb54
Mark Fynes <mark.fynes@broadcom.com>
2024-11-19 10:52:09 -0500

    .

--- tests/run-add-route-tests.sh
+++ tests/run-add-route-tests.sh
@@ -1,11 +1,26 @@
 pushd projects
 
+if [ -n "$1" ]; then
+  testRoutesList="$1"
+else
+  testRoutesList="tests-list-added-routes.txt"
+fi
+
+echo "file = $testRoutesList"
+
+echo Delete project folders
+while IFS= read -r test1; do
+    rm -fr ./$test1
+    echo
+done < "../$testRoutesList"
+echo
+
 echo Generate projects
 while IFS= read -r test1; do
     echo "Generating Project $test1"
     tanzu acc generate ta-scg-multi-route-configurator --local-server --output-dir ./$test1 --options-file ../$test1.json --extract
     echo
-done < "../tests-list-added-routes.txt"
+done < "../$testRoutesList"
 echo
 
 echo Deploy projects
@@ -18,7 +33,7 @@ while IFS= read -r test2; do
     sleep 5
     popd
     echo
-done < "../tests-list-added-routes.txt"
+done < "../$testRoutesList"
 echo
 
 
