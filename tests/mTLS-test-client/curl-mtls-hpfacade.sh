5835d62a08d7aae6b193a7e71d6abaae4dacf1e7
Mark Fynes <mark.fynes@broadcom.com>
2024-11-21 15:16:12 -0500

    .

--- tests/mTLS-test-client/curl-mtls-hpfacade.sh
+++ tests/mTLS-test-client/curl-mtls-hpfacade.sh
@@ -1,7 +1,14 @@
 # hp app returns all received request headers as response body
 # hpfacade SCG protects hp app, demanding client-side certificate with CN=test-client.fynesy.com
+tar zxvf ./test-client-demo-cert-key.zip
 # pass test
+echo
+echo PASS TEST
+echo
 curl -vvv --cert cert1.pem --key privkey1.pem https://hpfacade.homelab.fynesy.com/certcheck
-
+echo 
+echo
+echo FAIL TEST
+echo
 # fail test
 curl -vvv  https://hpfacade.homelab.fynesy.com/certcheck
